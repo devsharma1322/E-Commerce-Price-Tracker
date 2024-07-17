2b49ab7f42ca507ea044ef7795a34e7b345a1208
dev sharma <devsh1322@gmail.com>
2024-07-17 21:46:27 +0530

    Product details Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image"
+import { getAllProducts } from "@/lib/actions";
+import Product from "@/lib/models/product.model";
+import ProductCard from "@/components/ProductCard";
 
 
 const Home = async () => {
-  const allProducts = ['Apple Iphone 15' , 'Book', 'Sneakers'];
+  const allProducts = await getAllProducts();
 
   return (
     <>
@@ -41,8 +44,8 @@ const Home = async () => {
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {allProducts.map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key = {product._id} product = {product} />
           ))}
         </div>
       </section>
